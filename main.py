43ca04cb132418427e9ab2d720e52e3177c7fe6f
younes_qasempour <qasempouryounes@gmail.com>
2025-02-25 22:35:29 +0330

    Initial commit

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ response.raise_for_status()
 data = response.json()
 longitude = data["iss_position"]["longitude"]
 latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(iss_position)
